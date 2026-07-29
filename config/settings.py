8abf25001612268c0765525110caed6df80e1f97
Chinmayee259 <chinmayeeburte25@gmail.com>
2026-07-29 20:40:42 +0530

    base.html

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],  
+        # WHY?
+        # "We add the templates folder to Django's search path so render() can find HTML templates from a common location."
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+

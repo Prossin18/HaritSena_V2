e1e94e173e6682924aa7a3ea60710ea3ba014ed7
prossin18 <sangaleatul143@gmail.com>
2026-07-30 16:17:48 +0530

    design

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>welcpme to the jungal</h1>")   
+    return render(request,'home/index.html')   
 
 
 

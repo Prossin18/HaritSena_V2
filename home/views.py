a85c763eff1890a05cf641248e96c3c1c7c0ee4a
prossin18 <sangaleatul143@gmail.com>
2026-08-12 18:51:59 +0530

    about.html

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home/index.html')   
+    return render(request,'home/index.html')  
+
+def about(request):
+    return render(request,'home/about.html') 
 
 
 
